3eaf51436ff5c8a84c88e7367b642b3294bc2e1f
jiqianwanbaichi <52997120+jiqianwanbaichi@users.noreply.github.com>
2023-05-04 23:45:21 -0400

    Add files via upload

---
+++ routes/chat.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const { Configuration, OpenAIApi } = require("openai");
+const User = require('../models/User')
+
+const isLoggedIn = (req, res, next) => {
+    if (res.locals.loggedIn) {
+        next()
+    } else {
+        res.redirect('/login')
+    }
+}
+const configuration = new Configuration({
+    // apiKey: process.env.OPENAI_API_KEY
+    apiKey: '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'
+});
+const openai = new OpenAIApi(configuration);
+router.get('/form/:id', isLoggedIn, async (req, res, next) => {
+    const id = req.params.id;
+    console.log(id);
+    const user = await User.findById(id);
+    console.log(user);
+    res.render('form', { chat: user.chat });
+});
+router.post('/ask', async (req, res, next) => {
+    const { content, id } = req.body;
+    await User.findByIdAndUpdate(id, {
+        $addToSet: { chat: content }
+    })
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: content,
+        max_tokens: 200,
+    });
+    console.log(completion.data.choices);
+    res.json({ res: completion.data.choices[0].text });
+})
+module.exports = router;
+
+
